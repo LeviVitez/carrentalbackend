7eab1cf885074e03b2325ead6238c5d318a3dfe4
LeviVitez <bmwyolo555@gmail.com>
2023-06-01 16:01:55 +0200

    xd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { find } from 'rxjs';
 import Cars from './entities/cars.entity';
 import { isNotEmpty } from 'class-validator';
+import { faker } from '@faker-js/faker';
 
 @Controller()
 export class AppController {
@@ -28,12 +29,26 @@ export class AppController {
     newcar.brand=cars.brand
     newcar.model=cars.model
     newcar.daily_cost=cars.daily_cost
-
     const now=new Date().toISOString().substring(0,10)
-
     newcar.created_at=now
     newcar.updated_at=now
     await allCars.save(newcar)
     return newcar
+  }
+
+  @Post('seed')
+  async seed(@Body() cars: Cars) {
+    const allCars= this.dataSource.getRepository(Cars);
+    const newcar= new Cars();
+    cars.id=undefined;
+    newcar.license_plate_number = faker.number.octal();
+    newcar.brand = faker.vehicle.manufacturer();
+    newcar.model =faker.vehicle.model();
+    newcar.daily_cost = faker.number.int(5);
+    const now=new Date().toISOString().substring(0,10);
+    newcar.created_at=now;
+    newcar.updated_at=now;
+    await allCars.save(newcar);
+    return newcar;
   }
 }
